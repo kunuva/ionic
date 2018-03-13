95f3912d3827cdde51b9d96cc25f86f1d2242278
jcmbass77 <31079712+jcmbass77@users.noreply.github.com>
2018-03-13 22:09:50 +1000

    First GIT Commit

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -4,6 +4,10 @@ import { NavController, NavParams } from 'ionic-angular';
 
 import { ItemDetailsPage } from '../item-details/item-details';
 
+import { ActionSheetController } from 'ionic-angular';
+
+import { LoadingController } from 'ionic-angular';
+
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
@@ -12,9 +16,9 @@ export class ListPage {
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public loadingCtrl: LoadingController) {
     this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+    'american-football', 'boat', 'bluetooth', 'build','heart'];
 
     this.items = [];
     for(let i = 1; i < 11; i++) {
@@ -31,4 +35,13 @@ export class ListPage {
       item: item
     });
   }
+
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Please wait...",
+      duration: 3000
+    });
+    loader.present();
+  }
 }
+
